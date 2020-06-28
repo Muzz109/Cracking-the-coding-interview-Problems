25a0c7abb3077912e9d1a89163f9ca6c146be8eb
Muzz109 <58022822+Muzz109@users.noreply.github.com>
2020-06-27 23:47:29 -0700

    Update 1.6StringCompression.js

--- 1.6StringCompression.js
+++ 1.6StringCompression.js
@@ -1,1 +1,22 @@
+let arr ="aabcccccaaa";
 
+
+var cmpresdstr = function(arr) {
+
+  let str ="";
+   count =1;
+  for(let i=0;i< arr.length;i++){
+
+    if(arr[i]== arr[i+1]){
+     
+     count++;
+    }else{
+      str += arr[i]+count
+      count=1
+    }
+  }
+  return str
+    
+};
+
+cmpresdstr(arr)
